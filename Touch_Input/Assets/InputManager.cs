a75462b3ebc81f2cec77bcc49234f60d977aa26c
Chris Williams <chris@glowup.games>
2022-07-26 20:01:28 -0400

    Touch Input demo project ready

---
+++ Touch_Input/Assets/InputManager.cs
@@ -0,0 +1,80 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.EnhancedTouch;
+
+[DefaultExecutionOrder(-1)]
+public class InputManager : MonoBehaviour
+{
+    public static InputManager Instance;
+
+    public delegate void StartTouchEvent(Vector2 position, float time);
+    public event StartTouchEvent OnStartTouch;
+
+    public delegate void EndTouchEvent(Vector2 position, float time);
+    public event EndTouchEvent OnEndTouch;
+
+    public delegate void TouchMovedEvent(Vector2 position, float time);
+    public event TouchMovedEvent OnTouchMoved;
+
+    private void Awake()
+    {
+        if(Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnEnable()
+    {
+        TouchSimulation.Enable();
+        EnhancedTouchSupport.Enable();
+        UnityEngine.InputSystem.EnhancedTouch.Touch.onFingerDown += FingerDown;
+        UnityEngine.InputSystem.EnhancedTouch.Touch.onFingerUp += FingerUp;
+        UnityEngine.InputSystem.EnhancedTouch.Touch.onFingerMove += FingerMoved;
+    }
+
+    
+
+    private void OnDisable()
+    {
+        TouchSimulation.Disable();
+        EnhancedTouchSupport.Disable();
+        UnityEngine.InputSystem.EnhancedTouch.Touch.onFingerDown -= FingerDown;
+        UnityEngine.InputSystem.EnhancedTouch.Touch.onFingerUp -= FingerUp;
+        UnityEngine.InputSystem.EnhancedTouch.Touch.onFingerMove -= FingerMoved;
+    }
+
+
+    private void FingerDown(Finger finger)
+    {
+        Debug.Log("Touch position " + finger.screenPosition);
+
+        if (OnStartTouch != null)
+        {
+            OnStartTouch(finger.screenPosition, Time.time);
+        }
+    }
+
+    private void FingerUp(Finger finger)
+    {
+        Debug.Log("Touch ended");
+
+        if (OnEndTouch != null)
+        {
+            OnEndTouch(finger.screenPosition, Time.time);
+        }
+    }
+
+    private void FingerMoved(Finger finger)
+    {
+        if(OnTouchMoved != null)
+        {
+            OnTouchMoved(finger.screenPosition, Time.time);
+        }
+    }
+}
---
+++ Touch_Input/Assets/TouchInputTest.cs
@@ -0,0 +1,65 @@
+using System;
+using UnityEngine;
+
+public class TouchInputTest : MonoBehaviour
+{
+    // To cache references for better perfromance
+    private Camera mainCamera;
+    private InputManager inputManager;
+
+
+    private float startTouchTime;
+
+    private void Awake()
+    {
+        inputManager = InputManager.Instance;
+        mainCamera = Camera.main;
+    }
+
+    private void OnEnable()
+    {
+        // Subscribe to the OnStartTouchEvent
+        inputManager.OnStartTouch += Move;
+        inputManager.OnEndTouch += GetTime;
+        inputManager.OnTouchMoved += Drag;
+    }
+
+    private void GetTime(Vector2 position, float time)
+    {
+        float duration = time - startTouchTime;
+        Debug.Log("Duration: " + duration);
+    }
+
+    private void OnDisable()
+    {
+        // Unsubscribe to the OnStartTouchEvent
+        inputManager.OnStartTouch -= Move;
+        inputManager.OnEndTouch -= GetTime;
+        inputManager.OnTouchMoved -= Drag;
+    }
+    
+    public void Move(Vector2 screenPosition, float time)
+    {
+        transform.position = GetWorldCoordinates(screenPosition);
+        startTouchTime = time;
+    }
+
+    public void Drag(Vector2 screenPosition, float time)
+    {
+        transform.position = GetWorldCoordinates(screenPosition);
+    }
+
+    Vector3 GetWorldCoordinates(Vector2 screenPos)
+    {
+        // We want to convert our screen position to world coordinates
+
+        // For Z-axis, put the distance the camera has from the
+        // plane you are trying to touch
+        Vector3 screenCoord = new Vector3(screenPos.x, screenPos.y, mainCamera.nearClipPlane);
+
+        Vector3 worldCoord = mainCamera.ScreenToWorldPoint(screenCoord);
+        worldCoord.z = 0;
+
+        return worldCoord;
+    }
+}
